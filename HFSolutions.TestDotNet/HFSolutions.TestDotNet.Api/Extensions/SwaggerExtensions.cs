420e3a5c74755981f7b0285ca11406d7bb805a6e
jahns <jahns.saez@gmail.com>
2025-01-16 22:29:57 -0300

    feat: Se reafactoriza program.cs para mejor lectura

---
+++ HFSolutions.TestDotNet/HFSolutions.TestDotNet.Api/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+namespace HFSolutions.TestDotNet.Api.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerWithAuthentication(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token JWT",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = JwtBearerDefaults.AuthenticationScheme
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ HFSolutions.TestDotNet/HFSolutions.TestDotNet.Api/Extensions/ValidatorsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using HFSolutions.TestDotNet.Application.Dtos.UserDtos;
+using HFSolutions.TestDotNet.Application.Dtos.UserTaskDto;
+using HFSolutions.TestDotNet.Application.Validators.UserTaskValidators;
+using HFSolutions.TestDotNet.Application.Validators.UserValidators;
+
+namespace HFSolutions.TestDotNet.Api.Extensions
+{
+    public static class ValidatorsExtensions
+    {
+        public static IServiceCollection AddValidators(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<CreateUserDto>, CreateUserDtoValidator>();
+            services.AddScoped<IValidator<UserSecureDto>, UserSecureDtoValidator>();
+            services.AddScoped<IValidator<CreateUserTaskDto>, CreateUserTaskDtoValidator>();
+            services.AddScoped<IValidator<UpdateUserTaskDto>, UpdateUserTaskDtoValidator>();
+
+            return services;
+        }
+    }
+}
