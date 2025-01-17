4c9748df2623ca0055b07f8873cce7e74df324d3
jahns <jahns.saez@gmail.com>
2025-01-17 00:11:44 -0300

    test: Se crean test unitarios minimos

---
+++ HFSolutions.TestDotNet/HFSolutions.TestDotNet.Tests/Fixtures/ServiceProviderFixture.cs
@@ -0,0 +1,42 @@
+﻿using HFSolutions.TestDotNet.Tests.Application.Interfaces;
+using HFSolutions.TestDotNet.Tests.Application.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HFSolutions.TestDotNet.Tests.Fixtures
+{
+    public class ServiceProviderFixture
+    {
+        public IServiceProvider ServiceProvider { get; set; }
+
+        public ServiceProviderFixture()
+        {
+            var configuration = CreateConfiguration();
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddLogging();
+            serviceCollection.AddSingleton(configuration);
+            serviceCollection.AddHttpClient<IUserServiceTests, UserServiceTests>(client =>
+            {
+                string apiUrl = configuration.GetRequiredSection("ApiUrls:Base").Get<string>()!;
+
+                client.BaseAddress = new Uri(apiUrl);
+            });
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            ServiceProvider = serviceProvider;
+        }
+
+        private static IConfiguration CreateConfiguration()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            return configuration;
+        }
+    }
+}
---
+++ HFSolutions.TestDotNet/HFSolutions.TestDotNet.Tests/UserTests/LoginTests.cs
@@ -0,0 +1,34 @@
+﻿using HFSolutions.TestDotNet.Tests.Application.Interfaces;
+using HFSolutions.TestDotNet.Tests.Fixtures;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace HFSolutions.TestDotNet.Tests.UserTests
+{
+    [Collection(nameof(ServiceProviderCollection))]
+    public class LoginTests
+    {
+        private readonly ITestOutputHelper _testOutput;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IConfiguration _configuration;
+        private readonly IUserServiceTests _userServiceTests;
+
+        public LoginTests(ITestOutputHelper testOutput, ServiceProviderFixture serviceProviderFixture)
+        {
+            _testOutput = testOutput;
+            _serviceProvider = serviceProviderFixture.ServiceProvider;
+            _configuration = _serviceProvider.GetRequiredService<IConfiguration>();
+            _userServiceTests = _serviceProvider.GetRequiredService<IUserServiceTests>();
+        }
+
+        [Theory]
+        [InlineData("testusername3", "testusername3")]
+        public async Task Login_Should_Get_JWT_Token(string username, string password)
+        {
+            string token = await _userServiceTests.Login(username, password);
+
+            Assert.NotEmpty(token);
+        }
+    }
+}
